dd553d29c8615304fb3f80d959be92480dc64d96
TheArchitekt <richardpennymon@gmail.com>
2022-11-15 14:15:45 -0500

    Completed Server Python File

--- server.py
+++ server.py
@@ -18,7 +18,32 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """<!doctype html>
+              <html>
+                  Hi! This is the home page. Would you like a compliment or insult?
+                  <a href='/hello'>Compliment</a>
+                  <a href='/diss'>Insult</a>
+              </html>"""
+
+@app.route('/diss')
+def say_diss():
+    """say hello and diss user"""
+
+    player = request.args.get("person")
+
+    insult = request.args.get("insult")
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {insult}!
+      </body>
+    </html>
+    """
 
 
 @app.route('/hello')
@@ -35,12 +60,38 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
+          <select name='compliment'>
+            <option value='awesome'>Awesome</option>
+            <option value='terrific'>Terrific</option>
+            <option value='fantastic'>Fantastic</option>
+            <option value='neato'>Neato</option>
+            <option value='fantabulous'>Fantabulous</option>
+            <option value='wowza'>Wowza</option>
+            <option value='oh-so-not-meh'>Oh-so-not-meh</option>
+            <option value='brilliant'>Brilliant</option>
+            <option value='ducky'>Ducky</option>
+            <option value='coolio'>Coolio</option>
+            <option value='incredible'>Incredible</option>
+            <option value='wonderful'>Wonderful</option>
+            <option value='smashing'>Smashing</option>
+            <option value='lovely'>Lovely</option>
+          </select>
+          <input type="submit" value="Submit">
+        </form>
+        <form action="/diss">
+          What's your name? <input type="text" name="person">
+          <select name='insult'>
+            <option value='dumb'>Dumb</option>
+            <option value='ugly'>Ugly</option>
+            <option value='mean'>Mean</option>
+            <option value='oh-so-meh'>Oh-so-meh</option>
+            <option value='boring'>Boring</option>
+          </select>
           <input type="submit" value="Submit">
         </form>
       </body>
     </html>
     """
-
 
 @app.route('/greet')
 def greet_person():
@@ -48,7 +99,7 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliment")
 
     return f"""
     <!doctype html>
